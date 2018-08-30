821493f071a9d0b6cdcd08ffface989e91556a87
ltsok <ltsoksir@sina.com>
2018-08-30 23:59:31 +0800

    test-trunk

--- src/main/java/com/lts/course/controller/TestBootController.java
+++ src/main/java/com/lts/course/controller/TestBootController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-@RequestMapping("/testboot")
+@RequestMapping("/testboot44")
 public class TestBootController {
     @RequestMapping("/getuser")
     public User getUser() {
